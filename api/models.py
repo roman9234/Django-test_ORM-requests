5f24d69faff6bfe3aaaf0c5f9e96c0cd30efca45
roman9234 <roman_lesovoy@mail.ru>
2025-01-14 22:01:55 +0300

    created models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class BranchOffice(models.Model):
+    name = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=128)
+    floor = models.IntegerField()
+    branch_office = models.ForeignKey(BranchOffice, related_name="departments", on_delete=models.SET_NULL, null=True)
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=128)
+    position = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=20, null=True)
+    birth_date = models.DateTimeField(null=True)
+    email = models.EmailField(max_length=128, null=True)
+    department = models.ForeignKey(Department, related_name="employees", on_delete=models.CASCADE, null=True)
